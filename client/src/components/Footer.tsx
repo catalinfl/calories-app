fb833998d17de9ce8b447a5e61a48a4e9752cfaf
catalin <catalinfl02@gmail.com>
2023-08-19 00:01:52 +0300

    home page finished

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,29 +1,9 @@
-import React from 'react'
-
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-100 text-neutral-content">
-  <div>
-    <span className="footer-title">Services</span> 
-    <a className="link link-hover">Branding</a>
-    <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
-  </div> 
-  <div>
-    <span className="footer-title">Company</span> 
-    <a className="link link-hover">About us</a>
-    <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
-  </div> 
-  <div>
-    <span className="footer-title">Legal</span> 
-    <a className="link link-hover">Terms of use</a>
-    <a className="link link-hover">Privacy policy</a>
-    <a className="link link-hover">Cookie policy</a>
-  </div>
-</footer>
+    <footer className="footer p-6 flex flex-col justify-center items-center bg-neutral text-neutral-content">
+        <p className="text-center"> Made with &#10084; using React & Go Fiber </p>
+        <p className="text-center -mt-10"> 	 Catalin Flintasu &#169; 2023 </p>
+    </footer>
   )
 }
 
